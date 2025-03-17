d0f89b20c1906367ad0a56a4cdf59a9238646a06
Emerson Basilio <101227505+EmersonBLopes@users.noreply.github.com>
2025-03-17 20:32:16 -0300

    Add files via upload

---
+++ Exercicio_Estrutura_De_Repeticao_1.py
@@ -0,0 +1,8 @@
+#Exercício realizado no dia 17/03/2025
+
+numero = 1
+limite = int(input("Digite o numero maximo que sera contado:"))
+
+while numero < limite + 1:
+    print(numero)
+    numero += 1
